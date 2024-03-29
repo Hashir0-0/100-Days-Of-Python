13eda1d67b9d029e29d7fd4ba8ca466ab1925642
Hashir0-0 <hashirahmed254@gmail.com>
2024-03-29 23:56:50 +0500

    1st Day

---
+++ Day 1/Exercise 2.py
@@ -0,0 +1,18 @@
+#This code is about fixing different errors in the following python code
+
+#Fix the code below 👇
+
+'''print(Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+  print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backslash and n.")
+'''
+
+#Fixed code below 👇
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the '+' sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+
---
+++ Day 1/Exercise 3.py
@@ -0,0 +1,4 @@
+#Writing a program to give the no of characters in the name enter
+
+name=(len(input('What is your name ? ')))
+print(f'There are {name} characters in your name.')
